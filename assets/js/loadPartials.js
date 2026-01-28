6076cfe7b4f2ed847db099914879935ed0ffad17
Drishti Gupta <thedrishtigupta@gmail.com>
2026-01-28 22:07:35 +0530

    Feat: Partials header and footer, made some changes in links in index.html

---
+++ assets/js/loadPartials.js
@@ -0,0 +1,13 @@
+// Load Header
+fetch("partials/header.html")
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById("header").innerHTML = data;
+    });
+
+// Load Footer
+fetch("partials/footer.html")
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById("footer").innerHTML = data;
+    });
