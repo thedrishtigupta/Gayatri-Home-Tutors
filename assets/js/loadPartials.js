c101f4b2934f67f2fcec11d5e35e26f4636e607d
Drishti Gupta <thedrishtigupta@gmail.com>
2026-02-07 22:08:45 +0530

    Completed contact us page and section with loadpartials and proper css

--- assets/js/loadPartials.js
+++ assets/js/loadPartials.js
@@ -11,3 +11,11 @@ fetch("partials/footer.html")
     .then(data => {
         document.getElementById("footer").innerHTML = data;
     });
+
+
+
+fetch('partials/book-demo-form.html')
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById('book-demo-form-container').innerHTML = data;
+    });
